dacf37ad703100b2d6da0e91eda0909d8a495700
R2410165J <ephraimchikombe@gmail.com>
2025-03-27 15:12:12 +0200

    setup app routes

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+	title: 'Auth',
+	description: 'Authentication'
+}
+
+export default function RootLayout( { children}: { children: React.ReactNode}) {
+	return <div>{children}</div>
+}
