cf224b8cc3df927e6f05e5127ba3e3cbe30b0965
Mahere24 <maherel@zimstat.co.zw>
2025-03-28 20:07:12 +0200

    enable users to select flights and complete bookings

---
+++ app/(root)/flights/[id]/page.tsx
@@ -0,0 +1,11 @@
+import FlightBooking from '@/components/FlightBooking';
+
+const BookFlight = () => {
+  return (
+    <div>
+      <FlightBooking flight={} />
+    </div>
+  );
+};
+
+export default BookFlight;
---
+++ app/(root)/flights/page.tsx
@@ -0,0 +1,7 @@
+import FlightTable from '../../../components/FlightTable';
+
+const page = () => {
+  return <FlightTable />;
+};
+
+export default page;
